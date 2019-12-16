025bc0c94d583aec53e1603bfe0e96151a0ceaf4
LeRingue <42521105+LeRingue@users.noreply.github.com>
2019-12-16 17:25:31 +0100

    Ranking for group A in progress (draw not considered yet)

--- js/events.js
+++ js/events.js
@@ -1,62 +1,186 @@
 document.addEventListener('keydown', logKey);
 
-function logKey(e) {
-// Reset all previous computation
-let GroupRanking = [
-  {'group' : 'Groupe A', 'team' : A1, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0,},
-  {'group' : 'Groupe A', 'team' : A2, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0,},
-  {'group' : 'Groupe A', 'team' : A3, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0,},
-  {'group' : 'Groupe A', 'team' : A4, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0,}
+const matchNumberList = ["matchID1", "matchID2", "matchID3", "matchID4", "matchID5", "matchID6"];
+const scoreAList = ["score1A", "score2A", "score3A", "score4A", "score5A", "score6A"];
+const scoreBList = ["score1B", "score2B", "score3B", "score4B", "score5B", "score6B"];
+
+const GroupRankingInit = [
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A1, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A2, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A3, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A4, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0}
 ];
-// Check form entries that are set
-if ((document.getElementById("score1A").value != "") && (document.getElementById("score1B").value != "")){
-  let matchID = parseInt(document.getElementById("matchID").textContent, 10) - 1;
-  let score1 = parseInt(document.getElementById("score1A").value, 10);
-  let score2 = parseInt(document.getElementById("score1B").value, 10);
-  team1 = matchArray[matchID]['teamA'];
-  team2 = matchArray[matchID]['teamB'];
-  // Associate team with index in GroupRanking
-  let index1 = 0;
-  let index2 = 0;
-  while ((GroupRanking[index1]['team'] != team1) && (index1 < GroupRanking.length)){
-    index1 += 1;
+let GroupRanking = [
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A1, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A2, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A3, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0},
+  {'group' : 'Groupe A', 'rank' : 1, 'team' : A4, 'played' : 0, 'pts' : 0, 'BP' : 0, 'BM' : 0, 'GA' : 0}
+];
+
+function ComputeRank(){
+  // Check First
+  for (let i = 0; i < GroupRanking.length; i++){
+    for (let j = 0; j < GroupRanking.length; j++){
+      if (i == j){
+        continue;
+      }
+      if (GroupRanking[i]['pts'] < GroupRanking[j]['pts']) {
+        GroupRanking[i]['rank'] += 1;
+      }
+    }
   }
-  while ((GroupRanking[index2]['team'] != team2) && (index2 < GroupRanking.length)){
-    index2 += 1;
+  // Is there a draw for first place?
+  let countFirst = 0;
+  for (let i = 0; i < GroupRanking.length; i++){
+      if (GroupRanking[i]['rank'] == 1){
+        countFirst += 1;
+      }
   }
-  if(score1 > score2){
-    GroupRanking[index1]['pts'] += 3;
+  switch(countFirst){
+    case 1:
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
   }
-  if(score1 == score2){
-    GroupRanking[index1]['pts'] += 1;
-    GroupRanking[index2]['pts'] += 1;
+}
+
+  // samepts = [1:2, 1:3, 1:4, 2:3, 2:4, 3:4]
+  // let samepts =  [[0, 0, 0, 0],
+  //                 [0, 0, 0, 0],
+  //                 [0, 0, 0, 0]];
+  // for (i1 = 0; i < GroupRanking.length; i++){
+  //   for (i2 = 0; i < GroupRanking.length; i++){
+  //     if (GroupRanking[i1]['pts'] == GroupRanking[i2]['pts']){
+  //       samepts[i1][i2] = 1;
+  //     }
+  //   }
+  // }
+  // for (i = 0; i < GroupRanking.length; i++){
+  //   if (GroupRanking[0]['pts'] < GroupRanking[0+i]['pts']){
+  //     GroupRanking[0]['pts'] += 1;
+  //   }
+  // }
+//
+// }
+
+function logKey(e) {
+  // Reset GroupRanking
+  for (let i = 0; i < GroupRanking.length; i++){
+    GroupRanking[i]['group'] = GroupRankingInit[i]['group'];
+    GroupRanking[i]['rank'] = GroupRankingInit[i]['rank'];
+    GroupRanking[i]['team'] = GroupRankingInit[i]['team'];
+    GroupRanking[i]['played'] = GroupRankingInit[i]['played'];
+    GroupRanking[i]['pts'] = GroupRankingInit[i]['pts'];
+    GroupRanking[i]['BP'] = GroupRankingInit[i]['BP'];
+    GroupRanking[i]['BM'] = GroupRankingInit[i]['BM'];
+    GroupRanking[i]['GA'] = GroupRankingInit[i]['GA'];
   }
-  if(score1 < score2){
-    GroupRanking[index2]['pts'] += 3;
+  // Check form entries that are set
+  for (let num = 0; num < matchNumberList.length; num++){
+    scoreA = scoreAList[num];
+    scoreB = scoreBList[num];
+    MatchNumber = matchNumberList[num];
+    if ((document.getElementById(scoreA).value != "") && (document.getElementById(scoreB).value != "")){
+      let matchID = parseInt(document.getElementById(MatchNumber).textContent, 10) - 1;
+      let score1 = parseInt(document.getElementById(scoreA).value, 10);
+      let score2 = parseInt(document.getElementById(scoreB).value, 10);
+
+      let indexMID = 0;
+      while (indexMID < matchArray.length){
+        if ((matchArray[indexMID]['MatchID'] - 1) != matchID){
+          indexMID += 1;
+        }
+        else {
+          break;
+        }
+      }
+      team1 = matchArray[indexMID]['teamA'];
+      team2 = matchArray[indexMID]['teamB'];
+      // Populate MatchArray table with score
+      matchArray[indexMID]['scoreA'] = score1;
+      matchArray[indexMID]['scoreB'] = score2;
+
+      // Associate team with index in GroupRanking
+      let index1 = 0;
+      let index2 = 0;
+      while ((GroupRanking[index1]['team'] != team1) && (index1 < GroupRanking.length)){
+        index1 += 1;
+      }
+      while ((GroupRanking[index2]['team'] != team2) && (index2 < GroupRanking.length)){
+        index2 += 1;
+      }
+      if(score1 > score2){
+        GroupRanking[index1]['pts'] += 3;
+      }
+      if(score1 == score2){
+        GroupRanking[index1]['pts'] += 1;
+        GroupRanking[index2]['pts'] += 1;
+      }
+      if(score1 < score2){
+        GroupRanking[index2]['pts'] += 3;
+      }
+      GroupRanking[index1]['played'] += 1;
+      GroupRanking[index1]['BP'] += score1;
+      GroupRanking[index1]['BM'] += score2;
+      GroupRanking[index1]['GA'] += score1 - score2;
+      GroupRanking[index2]['played'] += 1;
+      GroupRanking[index2]['BP'] += score2;
+      GroupRanking[index2]['BM'] += score1;
+      GroupRanking[index2]['GA'] += score2 - score1;
+    }
   }
-  GroupRanking[index1]['BP'] += score1;
-  GroupRanking[index1]['BM'] += score2;
-  GroupRanking[index1]['GA'] += score1 - score2;
-  GroupRanking[index2]['BP'] += score2;
-  GroupRanking[index2]['BM'] += score1;
-  GroupRanking[index2]['GA'] += score2 - score1;
 
   // Compute rank
+  ComputeRank();
 
   // Update text to display
-  r1team.textContent = GroupRanking[index1]['team'];
-  r1pts.textContent = GroupRanking[index1]['pts'];
-  r1BP.textContent = GroupRanking[index1]['BP'];
-  r1BM.textContent = GroupRanking[index1]['BM'];
-  r1GA.textContent = GroupRanking[index1]['GA'];
+  let index = 0;
+  rank1.textContent = GroupRanking[index]['rank'];
+  r1team.textContent = GroupRanking[index]['team'];
+  r1played.textContent = GroupRanking[index]['played'];
+  r1pts.textContent = GroupRanking[index]['pts'];
+  r1BP.textContent = GroupRanking[index]['BP'];
+  r1BM.textContent = GroupRanking[index]['BM'];
+  r1GA.textContent = GroupRanking[index]['GA'];
 
-  r2team.textContent = GroupRanking[index2]['team'];
-  r2pts.textContent = GroupRanking[index2]['pts'];
-  r2BP.textContent = GroupRanking[index2]['BP'];
-  r2BM.textContent = GroupRanking[index2]['BM'];
-  r2GA.textContent = GroupRanking[index2]['GA'];
+  index += 1;
+  rank2.textContent = GroupRanking[index]['rank'];
+  r2team.textContent = GroupRanking[index]['team'];
+  r2played.textContent = GroupRanking[index]['played'];
+  r2pts.textContent = GroupRanking[index]['pts'];
+  r2BP.textContent = GroupRanking[index]['BP'];
+  r2BM.textContent = GroupRanking[index]['BM'];
+  r2GA.textContent = GroupRanking[index]['GA'];
 
-  r3team.textContent = GroupRanking[2]['team'];
-  r4team.textContent = GroupRanking[3]['team'];
-  }
+  index += 1;
+  rank3.textContent = GroupRanking[index]['rank'];
+  r3team.textContent = GroupRanking[index]['team'];
+  r3played.textContent = GroupRanking[index]['played'];
+  r3pts.textContent = GroupRanking[index]['pts'];
+  r3BP.textContent = GroupRanking[index]['BP'];
+  r3BM.textContent = GroupRanking[index]['BM'];
+  r3GA.textContent = GroupRanking[index]['GA'];
+
+  index += 1;
+  rank4.textContent = GroupRanking[index]['rank'];
+  r4team.textContent = GroupRanking[index]['team'];
+  r4played.textContent = GroupRanking[index]['played'];
+  r4pts.textContent = GroupRanking[index]['pts'];
+  r4BP.textContent = GroupRanking[index]['BP'];
+  r4BM.textContent = GroupRanking[index]['BM'];
+  r4GA.textContent = GroupRanking[index]['GA'];
+
+
+  // rank3.textContent = scoreA;
+  // r3team.textContent = scoreB;
+  //
+  // rank4.textContent = matchNumberList.length;
+  // r4team.textContent = Globo;//GroupRanking[3]['team'];
 }
--- js/static.js
+++ js/static.js
@@ -1,39 +1,144 @@
 // Teams
-const A1 = 'France';
-const A2 = 'Roumanie';
-const A3 = 'Albanie';
+const A1 = 'Turquie';
+const A2 = 'Italie';
+const A3 = 'Pays de Galles';
 const A4 = 'Suisse';
 
-const matchArray = [
-  {'group' : 'Groupe A', 'MatchID' : '1', 'date' : "10 juin 2018", 'time' : "21:00", 'teamA' : A1, 'teamB' : A2,},
-  {'group' : 'Groupe A', 'MatchID' : '2', 'date' : "11 juin 2018", 'time' : "15:00", 'teamA' : A3, 'teamB' : A4,},
-  {'group' : 'Groupe A', 'MatchID' : '3', 'date' : "15 juin 2018", 'time' : "18:00", 'teamA' : A2, 'teamB' : A4,},
-  {'group' : 'Groupe A', 'MatchID' : '4', 'date' : "16 juin 2018", 'time' : "20:00", 'teamA' : A1, 'teamB' : A3,},
-  {'group' : 'Groupe A', 'MatchID' : '5', 'date' : "17 juin 2018", 'time' : "17:00", 'teamA' : A1, 'teamB' : A4,},
-  {'group' : 'Groupe A', 'MatchID' : '6', 'date' : "18 juin 2018", 'time' : "19:00", 'teamA' : A2, 'teamB' : A3,},
+const B1 = 'Danemark';
+const B2 = 'Finland';
+const B3 = 'Belgique';
+const B4 = 'Russie';
+
+const C1 = 'Pays-Bas';
+const C2 = 'Ukraine';
+const C3 = 'Autriche';
+const C4 = 'PO Winner D';
+
+const D1 = 'Angleterre';
+const D2 = 'Croatie';
+const D3 = 'PO winner C';
+const D4 = 'République Tchèque';
+
+const E1 = 'Espagne';
+const E2 = 'Suède';
+const E3 = 'Pologne';
+const E4 = 'PO winner B';
+
+const F1 = 'PO winner A';
+const F2 = 'Portugal';
+const F3 = 'France';
+const F4 = 'Allemagne';
+
+
+// let matchArray = [
+//   {'group' : 'Groupe A', 'MatchID' : '1', 'date' : "12 juin 2018", 'time' : "21:00", 'teamA' : A1, 'teamB' : A2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe A', 'MatchID' : '2', 'date' : "13 juin 2018", 'time' : "15:00", 'teamA' : A3, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '3', 'date' : "13 juin 2018", 'time' : "18:00", 'teamA' : B1, 'teamB' : B2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '4', 'date' : "13 juin 2018", 'time' : "21:00", 'teamA' : B3, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '5', 'date' : "14 juin 2018", 'time' : "21:00", 'teamA' : C1, 'teamB' : C2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '6', 'date' : "14 juin 2018", 'time' : "18:00", 'teamA' : C3, 'teamB' : C4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '7', 'date' : "14 juin 2018", 'time' : "15:00", 'teamA' : D1, 'teamB' : D2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '8', 'date' : "15 juin 2018", 'time' : "15:00", 'teamA' : D3, 'teamB' : D4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '9', 'date' : "15 juin 2018", 'time' : "21:00", 'teamA' : E1, 'teamB' : E2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '10', 'date' : "15 juin 2018", 'time' : "18:00", 'teamA' : E3, 'teamB' : E4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '11', 'date' : "16 juin 2018", 'time' : "18:00", 'teamA' : F1, 'teamB' : F2, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '12', 'date' : "16 juin 2018", 'time' : "21:00", 'teamA' : F3, 'teamB' : F4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe A', 'MatchID' : '13', 'date' : "17 juin 2018", 'time' : "18:00", 'teamA' : A1, 'teamB' : A3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe A', 'MatchID' : '14', 'date' : "17 juin 2018", 'time' : "21:00", 'teamA' : A2, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '15', 'date' : "17 juin 2018", 'time' : "15:00", 'teamA' : B1, 'teamB' : B3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '16', 'date' : "18 juin 2018", 'time' : "18:00", 'teamA' : B2, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '17', 'date' : "18 juin 2018", 'time' : "21:00", 'teamA' : C1, 'teamB' : C3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '18', 'date' : "18 juin 2018", 'time' : "15:00", 'teamA' : C2, 'teamB' : C4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '19', 'date' : "19 juin 2018", 'time' : "18:00", 'teamA' : D1, 'teamB' : D3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '20', 'date' : "19 juin 2018", 'time' : "21:00", 'teamA' : D2, 'teamB' : D4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '21', 'date' : "19 juin 2018", 'time' : "15:00", 'teamA' : E1, 'teamB' : E3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '22', 'date' : "20 juin 2018", 'time' : "21:00", 'teamA' : E2, 'teamB' : E4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '23', 'date' : "20 juin 2018", 'time' : "15:00", 'teamA' : F1, 'teamB' : F3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '24', 'date' : "20 juin 2018", 'time' : "18:00", 'teamA' : F2, 'teamB' : F4, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe A', 'MatchID' : '25', 'date' : "21 juin 2018", 'time' : "18:00", 'teamA' : A4, 'teamB' : A1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe A', 'MatchID' : '26', 'date' : "21 juin 2018", 'time' : "18:00", 'teamA' : A2, 'teamB' : A3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '27', 'date' : "22 juin 2018", 'time' : "21:00", 'teamA' : B4, 'teamB' : B1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe B', 'MatchID' : '28', 'date' : "22 juin 2018", 'time' : "21:00", 'teamA' : B2, 'teamB' : B3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '29', 'date' : "22 juin 2018", 'time' : "18:00", 'teamA' : C4, 'teamB' : C1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe C', 'MatchID' : '30', 'date' : "22 juin 2018", 'time' : "18:00", 'teamA' : C2, 'teamB' : C3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '31', 'date' : "23 juin 2018", 'time' : "21:00", 'teamA' : D4, 'teamB' : D1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe D', 'MatchID' : '32', 'date' : "23 juin 2018", 'time' : "21:00", 'teamA' : D2, 'teamB' : D3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '33', 'date' : "24 juin 2018", 'time' : "18:00", 'teamA' : E4, 'teamB' : E1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe E', 'MatchID' : '34', 'date' : "24 juin 2018", 'time' : "18:00", 'teamA' : E2, 'teamB' : E3, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '35', 'date' : "24 juin 2018", 'time' : "21:00", 'teamA' : F4, 'teamB' : F1, 'scoreA' : "", 'scoreB' : ""},
+//   {'group' : 'Groupe F', 'MatchID' : '36', 'date' : "24 juin 2018", 'time' : "21:00", 'teamA' : F2, 'teamB' : F3, 'scoreA' : "", 'scoreB' : ""}
+// ];
+
+let matchArray = [
+  {'group' : 'Groupe A', 'MatchID' : '1', 'date' : "12 juin 2018", 'time' : "21:00", 'teamA' : A1, 'teamB' : A2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe A', 'MatchID' : '2', 'date' : "13 juin 2018", 'time' : "15:00", 'teamA' : A3, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe A', 'MatchID' : '13', 'date' : "17 juin 2018", 'time' : "18:00", 'teamA' : A1, 'teamB' : A3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe A', 'MatchID' : '14', 'date' : "17 juin 2018", 'time' : "21:00", 'teamA' : A2, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe A', 'MatchID' : '25', 'date' : "21 juin 2018", 'time' : "18:00", 'teamA' : A4, 'teamB' : A1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe A', 'MatchID' : '26', 'date' : "21 juin 2018", 'time' : "18:00", 'teamA' : A2, 'teamB' : A3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '3', 'date' : "13 juin 2018", 'time' : "18:00", 'teamA' : B1, 'teamB' : B2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '4', 'date' : "13 juin 2018", 'time' : "21:00", 'teamA' : B3, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '15', 'date' : "17 juin 2018", 'time' : "15:00", 'teamA' : B1, 'teamB' : B3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '16', 'date' : "18 juin 2018", 'time' : "18:00", 'teamA' : B2, 'teamB' : A4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '27', 'date' : "22 juin 2018", 'time' : "21:00", 'teamA' : B4, 'teamB' : B1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe B', 'MatchID' : '28', 'date' : "22 juin 2018", 'time' : "21:00", 'teamA' : B2, 'teamB' : B3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '5', 'date' : "14 juin 2018", 'time' : "21:00", 'teamA' : C1, 'teamB' : C2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '6', 'date' : "14 juin 2018", 'time' : "18:00", 'teamA' : C3, 'teamB' : C4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '17', 'date' : "18 juin 2018", 'time' : "21:00", 'teamA' : C1, 'teamB' : C3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '18', 'date' : "18 juin 2018", 'time' : "15:00", 'teamA' : C2, 'teamB' : C4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '29', 'date' : "22 juin 2018", 'time' : "18:00", 'teamA' : C4, 'teamB' : C1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe C', 'MatchID' : '30', 'date' : "22 juin 2018", 'time' : "18:00", 'teamA' : C2, 'teamB' : C3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '7', 'date' : "14 juin 2018", 'time' : "15:00", 'teamA' : D1, 'teamB' : D2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '8', 'date' : "15 juin 2018", 'time' : "15:00", 'teamA' : D3, 'teamB' : D4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '19', 'date' : "19 juin 2018", 'time' : "18:00", 'teamA' : D1, 'teamB' : D3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '20', 'date' : "19 juin 2018", 'time' : "21:00", 'teamA' : D2, 'teamB' : D4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '31', 'date' : "23 juin 2018", 'time' : "21:00", 'teamA' : D4, 'teamB' : D1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe D', 'MatchID' : '32', 'date' : "23 juin 2018", 'time' : "21:00", 'teamA' : D2, 'teamB' : D3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '9', 'date' : "15 juin 2018", 'time' : "21:00", 'teamA' : E1, 'teamB' : E2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '10', 'date' : "15 juin 2018", 'time' : "18:00", 'teamA' : E3, 'teamB' : E4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '21', 'date' : "19 juin 2018", 'time' : "15:00", 'teamA' : E1, 'teamB' : E3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '22', 'date' : "20 juin 2018", 'time' : "21:00", 'teamA' : E2, 'teamB' : E4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '33', 'date' : "24 juin 2018", 'time' : "18:00", 'teamA' : E4, 'teamB' : E1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe E', 'MatchID' : '34', 'date' : "24 juin 2018", 'time' : "18:00", 'teamA' : E2, 'teamB' : E3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '11', 'date' : "16 juin 2018", 'time' : "18:00", 'teamA' : F1, 'teamB' : F2, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '12', 'date' : "16 juin 2018", 'time' : "21:00", 'teamA' : F3, 'teamB' : F4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '23', 'date' : "20 juin 2018", 'time' : "15:00", 'teamA' : F1, 'teamB' : F3, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '24', 'date' : "20 juin 2018", 'time' : "18:00", 'teamA' : F2, 'teamB' : F4, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '35', 'date' : "24 juin 2018", 'time' : "21:00", 'teamA' : F4, 'teamB' : F1, 'scoreA' : "", 'scoreB' : ""},
+  {'group' : 'Groupe F', 'MatchID' : '36', 'date' : "24 juin 2018", 'time' : "21:00", 'teamA' : F2, 'teamB' : F3, 'scoreA' : "", 'scoreB' : ""},
 ];
 
 // Fill match ID
 let matchID = document.querySelectorAll('.matchID');
 matchID.forEach(function(matchID, index){
-  matchID.textContent = matchArray[index]['MatchID'];
+  if(matchArray[index]['group'] == "Groupe A"){
+    matchID.textContent = matchArray[index]['MatchID'];
+  }
 })
 // Fill team names
 let teamA = document.querySelectorAll('.teamA');
 teamA.forEach(function(teamA, index){
-  teamA.textContent = matchArray[index]['teamA'];
+  if(matchArray[index]['group'] == "Groupe A"){
+    teamA.textContent = matchArray[index]['teamA'];
+  }
 })
 let teamB = document.querySelectorAll('.teamB');
 teamB.forEach(function(teamB, index){
-  teamB.textContent = matchArray[index]['teamB'];
+  if(matchArray[index]['group'] == "Groupe A"){
+    teamB.textContent = matchArray[index]['teamB'];
+  }
 })
 // Fill match dates
 let date = document.querySelectorAll('.date');
 date.forEach(function(date, index){
-  date.textContent = matchArray[index]['date'];
+  if(matchArray[index]['group'] == "Groupe A"){
+    date.textContent = matchArray[index]['date'];
+  }
 })
 // Fill match hours
 let hour = document.querySelectorAll('.hour');
 hour.forEach(function(hour, index){
-  hour.textContent = matchArray[index]['time'];
+  if(matchArray[index]['group'] == "Groupe A"){
+    hour.textContent = matchArray[index]['time'];
+  }
 })
